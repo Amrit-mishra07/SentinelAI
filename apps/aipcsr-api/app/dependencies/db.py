04ba5bac02ebc1fae462eb14e97bf021aa046aa2
Amrit-mishra07 <amritanshu8415@gmail.com>
2026-06-23 19:27:28 +0530

    Fix db-core sys.path for github actions

--- apps/aipcsr-api/app/dependencies/db.py
+++ apps/aipcsr-api/app/dependencies/db.py
@@ -2,7 +2,7 @@ import sys
 import os
 
 # Add db-core to sys.path
-db_core_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', '..', 'core', 'db-core'))
+db_core_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', '..', '..', 'core', 'db-core'))
 if db_core_path not in sys.path:
     sys.path.insert(0, db_core_path)
 
